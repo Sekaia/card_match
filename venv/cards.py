3dbdf4d8e5217f52e8c99f02a668b5318bf6c29c
Sekaia <gillotte.bm@gmail.com>
2022-08-06 16:37:08 -0500

    Updated the game to work, still need to fix some bugs

--- venv/cards.py
+++ venv/cards.py
@@ -6,9 +6,24 @@
 # __str__
 
 class Card:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, word, location):
+        self.card = word
+        self.location = location
+        self.matched = False
+
+    def __eq__(self, other):
+        return self.card == other.card
+
+    def __str__(self):
+        return self.card
 
 
 if __name__ == "__main__":
-    pass
+    card1 = Card("Egg", "A1")
+    card2 = Card("Egg", "B1")
+    card3 = Card("Hut", "D4")
+
+    print(card1 == card2)
+    print(card2 == card3)
+
+    print(card1)
