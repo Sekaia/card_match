1d05955d173a63c829761d668d652fcb60e8c724
Sekaia <gillotte.bm@gmail.com>
2022-08-06 12:45:04 -0500

    added pseudo-code as well as some layout

--- venv/cards.py
+++ venv/cards.py
@@ -1,2 +1,14 @@
 # card class
+# hold a card
+# matched or not?
+# location
+# __eq__
+# __str__
 
+class Card:
+    def __init__(self, name):
+        self.name = name
+
+
+if __name__ == "__main__":
+    pass
--- venv/game.py
+++ venv/game.py
@@ -1,22 +1,40 @@
+import cards
 # game class
+# grid size
+# card options
+# columns
+# locations
+# methods
+    # create cards
+    # create grid
+    # check for matches
+    # check if game is won
+    # run the game
+# dunder main
+    # create game instance
+    # call start game
 
-# Memory Game
-#  |  A  |  B  |  C  |  D  |
-# 1|     |     |     |     |
-# 2|     |     |     |     |
-# 3|     |     |     |     |
-# 4|     |     |     |     |
+class Game:
+    def __init__(self, rows, columns):
+        self.rows = rows
+        self.columns = columns
 
-# cards hidden when game starts, but allow player to enter a spot, example: A3
-# and then a second spot, example: C2
-# then we will flip the cards:
+    def create_cards(self):
+        pass
 
-#  |  A  |  B  |  C  |  D  |
-# 1|     |     |     |     |
-# 2|     |     | Py  |     |
-# 3| Py  |     |     |     |
-# 4|     |     |     |     |
+    def create_grid(self):
+        pass
 
-# if they dont match, try again.
-# if they match, keep them flipped over.
-# when all cards are flipped, print game over
+    def check_matches(self):
+        pass
+
+    def check_for_win(self):
+        pass
+
+    def run_game(self):
+        pass
+
+
+if __name__ == "__main__":
+    new_game = Game(4, 4)
+    new_game.run_game()
